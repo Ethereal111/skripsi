a550fda4a8498cd03b53747a897650e4d9a1762a
septi249@gmail.com <suliez.tiana16@gmail.com>
2023-04-15 19:26:40 +0700

    kalkulator

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const kbackgroundColor = Colors.white;
+const kblueColor = Color.fromARGB(184, 13, 0, 29);
+const ktranparentColor = Color.fromARGB(255, 1, 94, 55);
